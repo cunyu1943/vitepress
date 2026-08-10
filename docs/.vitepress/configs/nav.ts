59324c145d832eb89fff2eb153ce5dd835f14401
村雨遥 <cunyu1024@gmail.com>
2026-08-10 21:02:37 +0800

    chore: 更新版权持有者并调整导航配置
    1. 将LICENSE中的版权持有者修改为cunyu1943
    2. 移除旧的年刊归档注释导航项
    3. 新增JetBrains IDEs中文教程导航链接

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -3,16 +3,6 @@ import type { DefaultTheme } from 'vitepress'
 export const nav: DefaultTheme.Config['nav'] = [
     { text: '🏠 首页', link: '/' },
     { text: ' 我的主页', link: 'https://cunyu1943.github.io' },
-    // { text: '🐎 2026 年刊', link: '/2026/latest' },
-
-    // {
-    //     text: '🗃️ 往昔留档',
-    //     items: [
-    //         { text: '🐍 2025 年刊', link: '/archive/2025/1228-0103' },
-    //         { text: '🐉 2024 年刊', link: '/archive/2024/0101-0105' },
-    //         { text: '🐇 2023 年刊', link: '/archive/2023/0415-0421' },
-    //     ]
-    // },
     {
         text: '📋 开源项目',
         items: [
@@ -22,6 +12,7 @@ export const nav: DefaultTheme.Config['nav'] = [
             { text: '💾 MySQL 学习指南', link: 'https://github.com/cunyu1943/mysql-tutorial' },
             { text: '🐍 Python 学习指南', link: 'https://github.com/cunyu1943/python-tutorial' },
             { text: '🖥️ 计算机学习指南', link: 'https://github.com/cunyu1943/cs-guide' },
+            { text: '🧰 JetBrains IDEs 中文教程', link: 'https://github.com/cunyu1943/jetbrains-ides-tutorial' },
             { text: '🍃 Spring Boot Demos', link: 'https://github.com/cunyu1943/spring-boot-demos' },
 
         ]
